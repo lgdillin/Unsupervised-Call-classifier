7ab3368b8a4f270578131b74468e8121637d36c2
Liam Dillingham <mailgd9@gmail.com>
2019-04-02 13:17:51 -0500

    did a LOT of work

--- R/athena.R
+++ R/athena.R
@@ -3,7 +3,6 @@
 athena.data = read.csv("../data/UA_AthenaData.csv", header = TRUE, sep = ",")
 
 # 6 minute timeframe
-# cut off at 0.99% (any # with calls >11 is a spam (cellphone), and >12 for landline)
 research.data = read.csv("../data/UA_ResearchData.csv", header=TRUE, sep = ",")
 isLandline.data = split(research.data, research.data$isLandline)
 
@@ -17,31 +16,54 @@ LLcount = aggregate(landline.data$call_time ~ landline.data$LineNumber, landline
 
 ## take the high-vol callers from research data and see if there are any behaviors in athena
 # filter for the anomalies (more than 11 phone calls in 6 minutes)
+# cut off at 0.99% (any # with calls >11 is a spam (cellphone), and >12 for landline)
 CPanomaly = CPcount[CPcount$`cellphone.data$call_time` > 11,]
 LLanomaly = LLcount[LLcount$`landline.data$call_time` > 12,]
 
+# Match the phone numbers from the resarch.data with the athena data
 CPathena = athena.data[CPanomaly$`cellphone.data$LineNumber`,]
 LLathena = athena.data[LLanomaly$`landline.data$LineNumber`,]
 
+# Strip off the categorial data and the cellphone numbers
+cp.numeric = CPathena[, -c(1,2)]
 
-test1 = CPathena[, -c(1,2)]
-test1 = log(test1 + eps)
-# apply function
-mads = apply(test1, 2, mad)
-output = data.frame(test1)
+# Compute the Median Absolute Deviation (mad)
+mads = apply(cp.numeric, 2, mad)
+y = names(cp.numeric[mads == 0])
+# If MAD=0, then var=0, so we remove this useless feature
+novariance = c()
+for(i in 1:length(mads)) {
+  if(mads[i] == 0) {
+    novariance = append(novariance, i)
+  }
+}
+cp.numeric = cp.numeric[, -novariance]
+mads = apply(cp.numeric, 2, mad) # Re-compute the MAD
 
-for(i in 1:ncol(test1)) {
-  col = test1[,i]
+# detect outliers using a modified-z-score approach
+output = data.frame(cp.numeric) # Copy the data for rewriting
+for(i in 1:ncol(cp.numeric)) {
+  col = cp.numeric[,i]
   col.med = median(col)
   col.mad = mads[i]
-  for(j in 1:nrow(test1)) {
-    x = test1[j,i]
+  for(j in 1:nrow(cp.numeric)) {
+    x = cp.numeric[j,i]
     output[j,i] = (0.6745 * (x - col.med)) / col.mad
   }
 }
 
+# Remove rows that contain outliers
+outliers = c()
+for(i in 1:nrow(output)) {
+  for(j in 1:ncol(output)) {
+    if(abs(output[i,j] > 3.5)) {
+      outliers = append(outliers, i)
+    }
+  }
+}
+removed.outliers = output[-outliers,]
 
-write.csv(test1, file = "test_transform.csv", row.names = F)
+write.csv(removed.outliers, file = "test_transform1.csv", row.names = F)
 
 write.csv(CPathena, file = "cellphone_athena_anomaly.csv", row.names = F)
 write.csv(LLathena, file = "landline_athena_anomaly.csv", row.names = F)
--- python/uaresearch.py
+++ python/uaresearch.py
@@ -8,23 +8,15 @@ import seaborn as sns
 from sklearn import preprocessing
 from sklearn.cluster import SpectralClustering, AgglomerativeClustering, DBSCAN
 from sklearn.decomposition import PCA
-from sklearn.manifold import Isomap, TSNE
+from sklearn.manifold import Isomap, TSNE, MDS
 # from sklearn.mixture import GMM
-from sklearn.neighbors.kde import KernelDensity
 from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
 
 plt.style.use('classic')
 
-cellphone_athena = pd.read_csv('./data/test_transform.csv')
+cellphone_athena = pd.read_csv('./data/test_transform1.csv')
 # landline_athena = pd.read_csv('./data/landline_athena_anomaly.csv')
-
 #cellphone_athena = cellphone_athena.drop(['LineNumber', 'CallCategory'], axis=1)
-
-
-#url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-# load dataset into Pandas DataFrame
-#cellphone_athena = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width', 'target'])
-#cellphone_athena = cellphone_athena.drop(['target'], axis=1)
 
 ## Scale the data to have mean=0 and unit variance:
 # scaler = RobustScaler().fit(cellphone_athena)
@@ -32,29 +24,36 @@ scaler = StandardScaler().fit(cellphone_athena)
 #scaler = MinMaxScaler().fit(cellphone_athena)
 scaler.transform(cellphone_athena)
 
-#Plotting the Cumulative Summation of the Explained Variance
+
+
+# Apply the transformation
+num_comp = 12
+pca = PCA(n_components=4)
+cellphone_transformed = pca.fit_transform(cellphone_athena)
+
+# Plot the variance elbow
 '''
+print(np.cumsum(pca.explained_variance_ratio_))
 plt.figure()
 plt.plot(np.cumsum(pca.explained_variance_ratio_))
 plt.xlabel('Number of Components')
+plt.xticks(np.arange(1, num_comp+1, 1.0))
 plt.ylabel('Variance (%)') #for each component
 plt.title('Explained Variance')
 plt.show()
 '''
 
-# Apply the transformation
-num_comp = 27
-pca = PCA()
-cellphone_transformed = pca.fit_transform(cellphone_athena)
-#cellphone_athena['PCA1'] = cellphone_transformed[:, 0]
-#cellphone_athena['PCA2'] = cellphone_transformed[:, 1]
-print(pca.get_covariance())
-print('==============================')
-#print(pca.components_.shape)
-# print(pca.components_)
+tsne = TSNE(n_components=2, random_state=1, perplexity=90, n_iter= 4000, learning_rate=800)
+reduced = tsne.fit_transform(cellphone_transformed)
+# reduced = MDS(n_components=2).fit_transform(cellphone_transformed)
+# reduced = umap.UMAP(n_neighbors=20, min_dist=0.15).fit_transform(cellphone_transformed)
 
-#sns.lmplot('PCA1', 'PCA2', data=cellp
-# hone_athena, fit_reg=False)
+cellphone_athena['DIM1'] = reduced[:, 0]
+cellphone_athena['DIM2'] = reduced[:, 1]
+sns.lmplot('DIM1', 'DIM2', data=cellphone_athena, fit_reg=False)
+plt.show()
+
+'''
 labels_list = range(0, num_comp)
 labels_names = []
 for i in labels_list:
@@ -66,15 +65,4 @@ plt.xticks(range(len(cellphone_athena.columns.tolist())),
     cellphone_athena.columns.tolist(),rotation=65,ha='left')
 plt.tight_layout()
 plt.show()# 
-
-
-#scaler1 = MinMaxScaler().fit(cellphone_transformed)
-#scaler1.transform(cellphone_transformed)
-#df = pd.DataFrame(cellphone_transformed)
-#df.to_csv('./data/PCA_trans.csv', index=False)
-
-#reduced = TSNE(n_components=1).fit_transform(cellphone_athena)
-#cellphone_athena['TSNE1'] = reduced[:, 0]
-#plt.plot(cellphone_athena['TSNE1'])
-#plt.show()
-
+'''
