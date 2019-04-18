a2d3d0cd3bb590c8d5da0a9826493c1edc7ac1c6
lgdillin <mailgd9@gmail.com>
2019-04-17 19:34:38 -0500

    more work

--- python/test_kmeans.py
+++ python/test_kmeans.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-# import umap
+import umap
 
 import matplotlib as mpl 
 import matplotlib.pyplot as plt 
@@ -26,6 +26,7 @@ num_comp = 4
 pca = PCA(n_components=num_comp)
 cellphone_transformed = pca.fit_transform(cellphone_athena)
 
+'''
 sum_of_squared_distances = []
 K = range(1,15)
 for k in K:
@@ -37,4 +38,17 @@ plt.plot(K, sum_of_squared_distances, 'bx-')
 plt.xlabel('k')
 plt.ylabel('Sum_of_squared_distances')
 plt.title('Elbow Method For Optimal k')
+plt.show()
+'''
+
+reduced = umap.UMAP(n_neighbors=20, min_dist=0.15).fit_transform(cellphone_transformed)
+
+cluster = KMeans(n_clusters = 4).fit(cellphone_transformed)
+labels = cluster.predict(cellphone_transformed)
+
+
+cellphone_athena['DIM1'] = reduced[:, 0]
+cellphone_athena['DIM2'] = reduced[:, 1]
+cellphone_athena['labels'] = labels
+sns.lmplot('DIM1', 'DIM2', data=cellphone_athena, hue='labels',fit_reg=False)
 plt.show()
